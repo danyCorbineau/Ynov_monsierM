71ee32329b574abdde3f1d25bb0f3d10c4d2cdd6
danyCorbineau <dany.corbineau@ynov.com>
2016-12-12 14:30:02 +0100

    Add files via upload

--- Eval/Danger.java
+++ Eval/Danger.java
@@ -1,4 +1,4 @@
-
+package Eval;
 import java.util.*;
 
 /**
@@ -6,10 +6,39 @@ import java.util.*;
  */
 public class Danger {
 
-    /**
-     * Default constructor
-     */
-    public Danger() {
+	private String nom;
+	private int pv;
+	private int attaque;
+	
+    public Danger(String line) {
+    	String[] temp=line.split(";");
+    	
+    	nom=temp[0];
+    	pv=Integer.valueOf(temp[1]);
+    	attaque=Integer.valueOf(temp[2]);
+    	
+    }
+    
+    public int attaquer()
+    {
+    	return this.attaque;
+    }
+    
+    public void perdPv(int pv) throws DangerMeurtException
+    {
+    	this.pv-=pv;
+    	if(pv<=0)
+    		throw new DangerMeurtException(this);
+    }
+    
+    
+    public String toString()
+    {
+		return nom+" "+pv+" "+attaque;
+    }
+    public String getName()
+    {
+    	return this.nom;
     }
 
 
