0f8a96d1d3266c3ad874c19a35c0f78b78d82306
nicodudu <nicodudu44@hotmail.fr>
2016-12-12 16:55:56 +0100

    Add files via upload

--- Eval/Carte.java
+++ Eval/Carte.java
@@ -1,41 +1,52 @@
-
+package commepackagenormal;
 import java.util.*;
+import java.io.*;
 
-/**
- * 
- */
+
 public class Carte {
 
-    /**
-     * Default constructor
-     */
-    public Carte() {
+
+    @SuppressWarnings("deprecation")
+	public Carte() throws FileNotFoundException {
+
+			DataInputStream dis = new DataInputStream(new FileInputStream(new File("carteTest.txt")));
+			try {
+				dis.readLine();
+			} catch (IOException e) {
+				System.out.println("Y'a une couille dans Carte !");
+				e.printStackTrace();
+			}
+			try {
+				dis.close();
+			} catch (IOException e) {
+				System.out.println("Problème à la fermeture de la carte !");
+				e.printStackTrace();
+			}
+
+		
     }
 
-    /**
-     * 
-     */
-    String nom;
-
-    /**
-     * 
-     */
-    List lieux;
-
-    /**
-     * 
-     */
-    List portes;
 
 
 
+    
 
-    /**
-     * @return
-     */
+	String nom;
+
+    List<Lieu> listLieux= new ArrayList<>();
+
+
+    List<Porte> listPortes= new ArrayList<>();
+
+
     void chargerCarte() {
-        // TODO implement here
-        return null;
+        
     }
+    
+    public int getSize()
+    {
+    	return this.listLieux.size();
+    }
+    
 
 }
