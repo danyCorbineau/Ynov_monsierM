cc7ecd5bbbcd1c202799feb625f5344e40816faa
danyCorbineau <dany.corbineau@ynov.com>
2016-12-14 20:46:25 +0100

    Add files via upload

--- Eval/DangerMeurtException.java
+++ Eval/DangerMeurtException.java
@@ -1,8 +1,15 @@
 package Eval;
+/*
+ * Dany CORBINEAU; dany.corbineau@ynov.com
+ * Classe DangerMeurtException: A utiliser pour générer une exeption si un danger n'a plus de PV
+ */
 
 public class DangerMeurtException extends Exception{
 	
-	
+	/**
+	 * constructeur de l'exception
+	 * @param d le danger qui n'a plus de pv
+	 */
 	DangerMeurtException(Danger d)
 	{
 		super("Danger "+d.getName());
