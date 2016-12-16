f10f4208a449624ad7e8dadf40c6417b1d19a670
JeremyYnov <jeremy.nunes@ynov.com>
2016-12-16 15:46:04 +0100

    Update MortPersonnageException.java

--- Eval/MortPersonnageException.java
+++ Eval/MortPersonnageException.java
@@ -1,19 +1,16 @@
 package p;
 
 /*
- * Jérémy NUNES
- * Cette classe est liée à la méthode de la classe "Personnage" : prendreDegats
- * Affiche en console la mort du personnage lorsque ses points de vie tombent à 0.
+ * JÃ©rÃ©my NUNES
+ * Cette classe est liÃ©e Ã  la mÃ©thode de la classe "Personnage" : prendreDegats
+ * Affiche en console la mort du personnage lorsque ses points de vie tombent Ã  0.
  */
 public class MortPersonnageException extends Exception {
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
 	public MortPersonnageException(){
-		super("Vous êtes mort.");
+		super("Vous Ãªtes mort.");
 	}
 
 }
